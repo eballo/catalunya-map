a9262cd119fe81ce01a33fac940a79bcaf93d9e5
eballo <enric.ballo@gmail.com>
2017-08-01 11:52:34 +0200

    update container name to support more than one

--- js/scale.raphael.js
+++ js/scale.raphael.js
@@ -1,5 +1,5 @@
 /*
- * ScaleRaphael 0.8 by Zevan Rosser 2010 
+ * ScaleRaphael 0.8 by Zevan Rosser 2010
  * For use with Raphael library : www.raphaeljs.com
  * Licensed under the MIT license.
  *
@@ -12,36 +12,36 @@
     wrapper.style.width = width + "px";
     wrapper.style.height = height + "px";
     wrapper.style.overflow = "hidden";
-    
+
     var nestedWrapper;
-      
+
     if (Raphael.type == "VML"){
       wrapper.innerHTML = "<rvml:group style='position : absolute; width: 1000px; height: 1000px; top: 0px; left: 0px' coordsize='1000,1000' class='rvml' id='vmlgroup'><\/rvml:group>";
       nestedWrapper = document.getElementById("vmlgroup");
     }else{
-      wrapper.innerHTML = "<div id='svggroup'><\/div>";
-      nestedWrapper = document.getElementById("svggroup");
+      wrapper.innerHTML = "<div id='svggroup-"+container+"'><\/div>";
+      nestedWrapper = document.getElementById("svggroup-"+container);
     }
- 
+
     var paper = new Raphael(nestedWrapper, width, height);
     var vmlDiv;
-    
+
     if (Raphael.type == "SVG"){
       paper.canvas.setAttribute("viewBox", "0 0 "+width+" "+height);
     }else{
       vmlDiv = wrapper.getElementsByTagName("div")[0];
     }
-    
+
     paper.changeSize = function(w, h, center, clipping){
       clipping = !clipping;
-      
+
       var ratioW = w / width;
       var ratioH = h / height;
       var scale = ratioW < ratioH ? ratioW : ratioH;
-      
+
       var newHeight = parseInt(height * scale);
       var newWidth = parseInt(width * scale);
-      
+
       if (Raphael.type == "VML"){
          // scale the textpaths
        var txt = document.getElementsByTagName("textpath");
@@ -56,7 +56,7 @@
             curr.style.font = curr._fontSize * scale + "px" + curr._font;
           }
         }
-        var newSize; 
+        var newSize;
         if (newWidth < newHeight){
          newSize = newWidth * 1000 / width;
         }else{
@@ -71,32 +71,32 @@
         }
         vmlDiv.style.overflow = "visible";
       }
-      
+
       if (clipping){
         newWidth = w;
         newHeight = h;
       }
-      
+
       wrapper.style.width = newWidth + "px";
       wrapper.style.height = newHeight + "px";
       paper.setSize(newWidth, newHeight);
-      
+
       if (center){
         wrapper.style.position = "absolute";
         wrapper.style.left = parseInt((w - newWidth) / 2) + "px";
         wrapper.style.top = parseInt((h - newHeight) / 2) + "px";
       }
     }
-    
+
     paper.scaleAll = function(amount){
       paper.changeSize(width * amount, height * amount);
     }
-    
+
     paper.changeSize(width, height);
-    
+
     paper.w = width;
     paper.h = height;
-    
+
     return paper;
   }
 })();
